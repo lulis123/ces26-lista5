af22c6c9bf41324b38d8700da28df93cdd4c4324
Luis <luisedumlg@gmail.com>
2020-11-29 01:33:48 -0300

    adicionando o ajax para checar a idade

--- public/script.js
+++ public/script.js
@@ -1,13 +1,29 @@
-$("#getDatabase").click(function(){
-    console.log("Oi");
-    $.ajax({
-        url: "http://localhost:8081/getDatabase",
-        method: "GET",
-        dataType:"json",
-        success: function(data){
-            console.log(data)
-            $('#databaseInfo').empty()
-            $('#databaseInfo').append(JSON.stringify(data))
-        }
-    })
+const isNumber = function(string){
+    if(string == null){
+        return false;
+    }
+    try{
+        const j = parseInt(string);
+    }catch{
+        return false;
+    }
+    return true;
+}
+
+$("#registerForm").submit(function(){
+
+    const age = $('input[name=age]').val();
+    const parseAge = parseInt(age);
+    if(isNaN(parseAge)){
+        $('#warningMessage').empty();
+        $('#warningMessage').append("Sua idade deve ser um número");
+    }
+    if(!isNaN(parseAge) && parseAge< 18){
+        $('#warningMessage').empty();
+        $('#warningMessage').append("Você deve ser maior de 18 anos!");
+    }
+    if(!isNaN(parseAge) && parseAge >= 18){
+        $('#warningMessage').empty();
+    }
+
 });
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
-const database = require('./static/database.json');
-const multer = require('multer');
+const $ = require('jquery');
 const app = express();
+
+
 
 app.use(express.static('public'))
 
@@ -14,9 +15,8 @@ app.get('/process_get', (req,res)=>{
     const response = {
         name:req.query.name,
         age: req.query.age
-    }
+    };
     console.log(response);
-    res.end(JSON.stringify(response))
 })
 
 app.listen('8081', () => console.log('App na porta 8081'));
